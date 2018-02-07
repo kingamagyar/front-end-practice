b1306e6020f0be9a9e9b784910cb59b7dc5059d4
Kinga Magyar <kinga.magyar89@gmail.com>
2018-02-07 23:46:59 +0100

    style corrected in number printer exercise

--- basic-js/Quiz10-print-numbers.js
+++ basic-js/Quiz10-print-numbers.js
@@ -2,7 +2,7 @@ console.log('Print all numbers between -10 and 19');
 
 var num1 = -10;
 
-while(num1 < 20){
+while (num1 < 20) {
 	console.log(num1);
 	num1++;
 }
@@ -11,8 +11,8 @@ console.log('Print all even numbers between 10 and 40');
 
 var num2 = 10;
 
-while(num2 < 41) {
-	if(num2 % 2 === 0){
+while (num2 < 41) {
+	if (num2 % 2 === 0) {
 		console.log(num2)
 	}
 	num2++;
@@ -22,8 +22,8 @@ console.log('Print all odd numbers between 300 and 333');
 
 var num3 = 300;
 
-while(num3 < 334) {
-	if(num3 % 2 !== 0){
+while (num3 < 334) {
+	if (num3 % 2 !== 0) {
 		console.log(num3);
 	}
 	num3++;
@@ -33,9 +33,9 @@ console.log('Print all numbers divisible by 5 AND 3 between 5 and 50');
 
 var num4 = 5;
 
-while(num4 < 51) {
-	if(num4 % 5 === 0 && num4 % 3 === 0){
-		console.log (num4);
+while (num4 < 51) {
+	if (num4 % 5 === 0 && num4 % 3 === 0) {
+		console.log(num4);
 	}
 	num4++;
 }
